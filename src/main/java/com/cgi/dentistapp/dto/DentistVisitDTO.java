f507dd923ab3fdfa04e415cf61daa257c1cdc3bb
Heiko Piirme <heiko.piirme@gmail.com>
2018-10-04 19:43:15 +0300

    Stage 1: Modified inputs, database connection

--- src/main/java/com/cgi/dentistapp/dto/DentistVisitDTO.java
+++ src/main/java/com/cgi/dentistapp/dto/DentistVisitDTO.java
@@ -12,7 +12,7 @@ public class DentistVisitDTO {
     String dentistName;
 
     @NotNull
-    @DateTimeFormat(pattern = "dd.MM.yyyy")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     Date visitTime;
 
     public DentistVisitDTO() {
